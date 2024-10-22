402f79dade720540b3a83a3102eb5cb2cf08065d
jandoniec <john@techmaxi.net>
2024-10-22 12:49:09 +0200

    Initial Commit

---
+++ RaceAsisstantTests/RaceAsisstantTests.swift
@@ -0,0 +1,16 @@
+//
+//  RaceAsisstantTests.swift
+//  RaceAsisstantTests
+//
+//  Created by Jan Doniec on 22/10/2024.
+//
+
+import Testing
+
+struct RaceAsisstantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
